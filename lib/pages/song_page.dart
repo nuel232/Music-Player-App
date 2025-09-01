4c6e3e34c71af05c8393229395913eac3c24eae0
nuel232 <nwankwoala3@gmail.com>
2025-09-01 15:21:10 +0100

    Refactor SongPage to improve state management and UI layout; integrate current song details and enhance playback controls with responsive design elements.

--- lib/pages/song_page.dart
+++ lib/pages/song_page.dart
@@ -9,107 +9,170 @@ class SongPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Consumer<PlaylistProvider>(
-      builder: (context, value, child) => Scaffold(
-        backgroundColor: Theme.of(context).colorScheme.surface,
-        body: SafeArea(
-          child: Padding(
-            padding: const EdgeInsets.only(left: 25.0, right: 25, bottom: 25),
-            child: Column(
-              children: [
-                //app bar
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    //back button
-                    IconButton(onPressed: () {}, icon: Icon(Icons.arrow_back)),
+      builder: (context, value, child) {
+        //get playlist
+        final playlist = value.playlist;
 
-                    //tittle
-                    Text('P L A Y L I S T'),
-
-                    //menu button
-                    IconButton(onPressed: () {}, icon: Icon(Icons.menu)),
-                  ],
-                ),
-
-                //album artwork'
-                NeuBox(
-                  child: Column(
+        //get current song index
+        final currentSong = playlist[value.currentSongIndex ?? 0];
+        return Scaffold(
+          backgroundColor: Theme.of(context).colorScheme.surface,
+          body: SafeArea(
+            child: Padding(
+              padding: const EdgeInsets.only(left: 25.0, right: 25, bottom: 25),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  //app bar
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
                     children: [
-                      //image
-                      ClipRRect(
-                        borderRadius: BorderRadius.circular(12),
-                        child: Image.asset('lib/assets/images/with-you.jpg'),
+                      //back button
+                      IconButton(
+                        onPressed: () => Navigator.pop(context),
+                        icon: Icon(Icons.arrow_back),
                       ),
 
+                      //tittle
+                      Text('P L A Y L I S T'),
+
+                      //menu button
+                      IconButton(onPressed: () {}, icon: Icon(Icons.menu)),
+                    ],
+                  ),
+
+                  SizedBox(height: 25),
+
+                  //album artwork'
+                  NeuBox(
+                    child: Column(
+                      children: [
+                        //image
+                        ClipRRect(
+                          borderRadius: BorderRadius.circular(12),
+                          child: Image.asset(currentSong.albumArtistImagePath),
+                        ),
+
+                        Padding(
+                          padding: const EdgeInsets.all(15.0),
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: [
+                              //song and artist name
+                              Column(
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children: [
+                                  Text(
+                                    currentSong.songName,
+                                    style: TextStyle(
+                                      fontSize: 18,
+                                      fontWeight: FontWeight.bold,
+                                    ),
+                                  ),
+                                  Text(
+                                    currentSong.artistName,
+                                    style: TextStyle(
+                                      fontSize: 14,
+                                      color: Colors.grey,
+                                    ),
+                                  ),
+                                ],
+                              ),
+
+                              Icon(Icons.favorite, color: Colors.red),
+                            ],
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+
+                  SizedBox(height: 30),
+
+                  //song duration progress
+                  Column(
+                    children: [
                       Padding(
-                        padding: const EdgeInsets.all(15.0),
+                        padding: const EdgeInsets.symmetric(horizontal: 25.0),
                         child: Row(
                           mainAxisAlignment: MainAxisAlignment.spaceBetween,
                           children: [
-                            //song and artist name
-                            Column(
-                              crossAxisAlignment: CrossAxisAlignment.start,
-                              children: [
-                                Text(
-                                  value.currentSong.songName,
-                                  style: TextStyle(
-                                    fontSize: 18,
-                                    fontWeight: FontWeight.bold,
-                                  ),
-                                ),
-                                Text(
-                                  value.currentSong.artistName,
-                                  style: TextStyle(
-                                    fontSize: 14,
-                                    color: Colors.grey,
-                                  ),
-                                ),
-                              ],
-                            ),
+                            //start and edit time
+                            Text('0:00'),
 
-                            Icon(Icons.favorite, color: Colors.red),
+                            //shuffle icon
+                            Icon(Icons.shuffle),
+
+                            //repeat icon
+                            Icon(Icons.repeat),
+
+                            //end time
+                            Text('3:30'),
                           ],
+                        ),
+                      ),
+
+                      //song duration progress
+                      SliderTheme(
+                        data: SliderTheme.of(context).copyWith(
+                          thumbShape: RoundSliderThumbShape(
+                            enabledThumbRadius: 1,
+                          ),
+                        ),
+                        child: Slider(
+                          min: 0,
+                          max: 100,
+                          value: 0,
+                          activeColor: Colors.green,
+                          inactiveColor: Colors.grey.shade400,
+                          onChanged: (value) {},
                         ),
                       ),
                     ],
                   ),
-                ),
 
-                SizedBox(height: 30),
+                  const SizedBox(height: 30),
 
-                //song duration progress
-                Padding(
-                  padding: const EdgeInsets.symmetric(horizontal: 25.0),
-                  child: Column(
+                  // playback controls
+                  Row(
                     children: [
-                      Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                        children: [
-                          //start and edit time
-                          Text('0:00'),
-
-                          //shuffle icon
-                          Icon(Icons.shuffle),
-
-                          //repeat icon
-                          Icon(Icons.repeat),
-
-                          //end time
-                          Text('3:30'),
-                        ],
+                      //skip previous
+                      Expanded(
+                        child: GestureDetector(
+                          onTap: () {},
+                          child: NeuBox(child: Icon(Icons.skip_previous)),
+                        ),
                       ),
 
-                      Slider(value: 0, onChanged: (value) {}),
+                      SizedBox(width: 25),
+
+                      //play pause
+                      Expanded(
+                        //this means it should be double in size
+                        flex: 2,
+                        child: GestureDetector(
+                          onTap: () {},
+                          child: NeuBox(child: Icon(Icons.play_arrow)),
+                        ),
+                      ),
+
+                      SizedBox(width: 25),
+
+                      //skip forward
+                      Expanded(
+                        child: GestureDetector(
+                          onTap: () {},
+                          child: NeuBox(child: Icon(Icons.skip_next)),
+                        ),
+                      ),
                     ],
                   ),
-                ),
-
-                // playback controls
-              ],
+                ],
+              ),
             ),
           ),
-        ),
-      ),
+        );
+      },
     );
   }
 }
